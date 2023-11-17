3798312592e0f7e86d01bbf282342957cf655492
Tae C <62630025+TaeChung@users.noreply.github.com>
2023-11-17 15:56:14 -0500

    added assets folder and content

---
+++ assets/db/schema.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+  
+);
+
+CREATE TABLE roles (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   title VARCHAR (30),
+   salary DECIMAL,
+   department_id INT,
+   FOREIGN KEY (department_id) 
+   REFERENCES department(id)
+   ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  roles_id INT,
+  manager_id INT,
+  FOREIGN KEY (roles_id) 
+  REFERENCES roles(id)
+  ON DELETE CASCADE
+ 
+);
+
+
+
+
---
+++ assets/db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO department (name)
+VALUES ("Accounting"),
+       ("Management"),
+       ("Investing");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("CEO", 1500000, 2),
+       ("Intern", 0, 2),
+       ("Actuary", 145000, 3),
+       ("Financial Analyst", 245000, 3),
+       ("Mint Treasurer", 300000, 3),
+       ("Accountant", 115000, 1);
+       
+INSERT INTO employee (first_name, last_name, roles_id)
+VALUES ("David", "King", 2),
+       ("Pedro", "Vasquez", 1),
+       ("Bill", ":Bose", 3),
+       ("Patricia", "Hammonds", 4),
+       ("Ozwald", "Helmsdottir", 5);
+
+
+
